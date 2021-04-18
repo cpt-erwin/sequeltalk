bf98b436fed2d0cfb0a0daacdea6cc253dde0748
Michal Tuček <xtucm003@studenti.czu.cz>
2021-04-18 17:02:24 +0200

    Added static reference of ans instance of itself Implemented translation of DEBUG environmental var to boolean Added debugging tool Moved tables from constructor to a custom method

--- src/App.php
+++ src/App.php
@@ -15,23 +15,30 @@ use PDOException;
  */
 class App
 {
+    /** @var App Store an instance of itself in itself to make it globally accessible */
+    public static App $app;
+
     /** @var PDO PDO connector */
-    private PDO $conn;
+    public PDO $conn;
 
     /** @var Table[]  */
-    private array $tables;
+    private array $tables = [];
 
     /**
      * App constructor.
-     * @param Table[] $tables
      * @throws ErrorException
      */
-    public function __construct(array $tables)
+    public function __construct()
     {
+        self::$app = $this;
+
         // FIXME: Better way to specify .env destination than this?
         // Initialize .env configuration
         $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
         $dotenv->load();
+
+        // Set $_ENV['DEBUG'] value as boolen instead of string
+        $_ENV['DEBUG'] = filter_var($_ENV['DEBUG'], FILTER_VALIDATE_BOOLEAN);
 
         try {
             $this->conn = new PDO("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
@@ -41,71 +48,16 @@ class App
             throw new ErrorException('Couldn\'t logged into the DB: ' . $e->getMessage());
             // Change this part to suit your needs
         }
-
-        $this->tables = $tables;
     }
 
     public function run(): void
     {
-        /* specifies, if you want to treat foreign keys in table as a object references */
-        $foreignKeyAsObject = false;
+        foreach ($this->tables as $table) {
+            echo "\"-------------- {$table->getObjectName()} object schema --------------\"<br>";
+            echo "<pre>" . $table->getObjectSchema() . "</pre>";
 
-        $database = "database_name";
-        $table = "table_name";
-        $variable = "smalltalk_variable_name";
-        $object = "smalltalk_object_name";
-        $skip = ['skip', 'specific', 'columns'];
-
-        /* EXAMPLE */
-        /*
-            $database = "eshop";
-            $table = "invoice";
-            $variable = "i";
-            $object = "Invoice";
-            $skip = ['order_id'];
-        */
-
-        $query = $this->conn->query("SELECT * FROM {$database}.{$table}");
-        foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $index => $row) {
-            $index++; // Use for variable number but start with 1 instead of 0
-            $smalltalk = "{$variable}{$index} := {$object} new.<br>{$variable}{$index} ";
-            foreach ($row as $column => $value) {
-                /* skip columns we don't want */
-                if (in_array($column, $skip)) continue;
-
-                if ($foreignKeyAsObject & strpos($column, '_id') !== false) {
-                    if (empty($value)) continue;
-                    $column = explode('_id', $column)[0];
-                    $smalltalk .= $column . ': ' . $column[0] . $value;
-                } else {
-                    if (strpos($column, 'id') !== false) continue; // Skip id collumns
-                    if (empty($value)) continue;
-
-                    /* if column name is like date generate smalltalk friendly date */
-                    $isDate = (strpos($column, 'date') !== false) || (strpos($column, 'Date') !== false) || (strpos($column, 'At') !== false);
-                    if ($isDate) {
-                        $value = explode('-', $value);
-                        $value = $value[1] . ' ' . $value[2] . ' ' . $value[0];
-                    }
-
-                    if (is_numeric($value)) {
-                        $smalltalk .= $column . ": " . $value . "";
-                    } else {
-                        $smalltalk .= $column . ": '" . $value . "'";
-                    }
-
-                    if ($isDate) {
-                        $smalltalk .= ' asDate';
-                    }
-                }
-
-                $smalltalk .= "; ";
-            }
-            //Remove the last character using substr
-            $smalltalk = substr($smalltalk, 0, -2);
-            $smalltalk .= '.<br>';
-
-            echo $smalltalk . "<br>";
+            echo "\"-------------- {$table->getObjectName()} records --------------\"<br><br>";
+            echo $table->getData() . "<br><br>";
         }
     }
 
@@ -145,5 +97,23 @@ class App
         $record .= '.';
 
         return $record;
+    }
+
+    /**
+     * Simple tool for debugging observed variables.
+     * @param string $tag
+     * @param mixed $content
+     */
+    public function debug(string $tag, $content): void
+    {
+        echo "<pre style='white-space: pre-wrap; background-color: #ebebeb; padding: .1rem 0 1rem 0;'>";
+        echo "<h3 style='text-align: center;'>{$tag}</h3><hr><div style='padding: 0 1rem;'>";
+        print_r($content);
+        echo "</div></pre>";
+    }
+
+    public function addTable(Table $table): void
+    {
+        $this->tables[] = $table;
     }
 }
