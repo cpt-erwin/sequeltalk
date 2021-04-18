6609a6fd870b1d93bd5a5cb1919b69c839bd780c
Michal Tuček <xtucm003@studenti.czu.cz>
2021-04-18 14:25:39 +0200

    Minor structural changes

--- src/App.php
+++ src/App.php
@@ -12,12 +12,15 @@ class App
     /** @var PDO PDO connector */
     private PDO $conn;
 
+    /** @var Table[]  */
+    private array $tables;
+
     /**
      * App constructor.
-     *
+     * @param Table[] $tables
      * @throws ErrorException
      */
-    public function __construct()
+    public function __construct(array $tables)
     {
         // FIXME: Better way to specify .env destination than this?
         // Initialize .env configuration
@@ -32,28 +35,14 @@ class App
             throw new ErrorException('Couldn\'t logged into the DB: ' . $e->getMessage());
             // Change this part to suit your needs
         }
+
+        $this->tables = $tables;
     }
 
     public function run(): void
     {
         /* specifies, if you want to treat foreign keys in table as a object references */
         $foreignKeyAsObject = false;
-
-        /* EXAMPE */
-        /*
-        Let's say we have a table invoice that has a foreign key billing_information_id.
-
-        If $foreignKeyAsObject is true it will generate the fallowing:
-
-        i1 := Invoice new.
-        i1 issueDate: '03 01 2021' asDate; dueDate: '04 01 2021' asDate; payDate: '03 02 2021' asDate; billing_information: b1.
-
-        If $foreignKeyAsObject is false it will generate the code without billing_information_id column (same as skip):
-
-        i1 := Invoice new.
-        i1 issueDate: '03 01 2021' asDate; dueDate: '04 01 2021' asDate; payDate: '03 02 2021' asDate.
-
-        */
 
         $database = "database_name";
         $table = "table_name";
@@ -70,75 +59,73 @@ class App
             $skip = ['order_id'];
         */
 
-        /* if true it will generate add: {$variable}{$index} from start to end */
-        /* useful if you want to generate code to add specific objects to some collection */
-        $manual = true;
-        $start = 1;
-        $end = 10;
+        $query = $this->conn->query("SELECT * FROM {$database}.{$table}");
+        foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $index => $row) {
+            $index++; // Use for variable number but start with 1 instead of 0
+            $smalltalk = "{$variable}{$index} := {$object} new.<br>{$variable}{$index} ";
+            foreach ($row as $column => $value) {
+                /* skip columns we don't want */
+                if (in_array($column, $skip)) continue;
 
-        /* manual generator for adding items into collection */
-        if ($manual) {
-            echo $this->collectionGenerator("c", $start, $end, $variable);
-            exit;
-        } else {
-            $query = $this->conn->query("SELECT * FROM {$database}.{$table}");
-            foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $index => $row) {
-                $index++; // Use for variable number but start with 1 instead of 0
-                $smalltalk = "{$variable}{$index} := {$object} new.<br>{$variable}{$index} ";
-                foreach ($row as $column => $value) {
-                    /* skip columns we don't want */
-                    if (in_array($column, $skip)) continue;
+                if ($foreignKeyAsObject & strpos($column, '_id') !== false) {
+                    if (empty($value)) continue;
+                    $column = explode('_id', $column)[0];
+                    $smalltalk .= $column . ': ' . $column[0] . $value;
+                } else {
+                    if (strpos($column, 'id') !== false) continue; // Skip id collumns
+                    if (empty($value)) continue;
 
-                    if ($foreignKeyAsObject & strpos($column, '_id') !== false) {
-                        if (empty($value)) continue;
-                        $column = explode('_id', $column)[0];
-                        $smalltalk .= $column . ': ' . $column[0] . $value;
-                    } else {
-                        if (strpos($column, 'id') !== false) continue; // Skip id collumns
-                        if (empty($value)) continue;
-
-                        /* if column name is like date generate smalltalk friendly date */
-                        $isDate = (strpos($column, 'date') !== false) || (strpos($column, 'Date') !== false) || (strpos($column, 'At') !== false);
-                        if ($isDate) {
-                            $value = explode('-', $value);
-                            $value = $value[1] . ' ' . $value[2] . ' ' . $value[0];
-                        }
-
-                        if (is_numeric($value)) {
-                            $smalltalk .= $column . ": " . $value . "";
-                        } else {
-                            $smalltalk .= $column . ": '" . $value . "'";
-                        }
-
-                        if ($isDate) {
-                            $smalltalk .= ' asDate';
-                        }
+                    /* if column name is like date generate smalltalk friendly date */
+                    $isDate = (strpos($column, 'date') !== false) || (strpos($column, 'Date') !== false) || (strpos($column, 'At') !== false);
+                    if ($isDate) {
+                        $value = explode('-', $value);
+                        $value = $value[1] . ' ' . $value[2] . ' ' . $value[0];
                     }
 
-                    $smalltalk .= "; ";
-                }
-                //Remove the last character using substr
-                $smalltalk = substr($smalltalk, 0, -2);
-                $smalltalk .= '.<br>';
+                    if (is_numeric($value)) {
+                        $smalltalk .= $column . ": " . $value . "";
+                    } else {
+                        $smalltalk .= $column . ": '" . $value . "'";
+                    }
 
-                echo $smalltalk . "<br>";
+                    if ($isDate) {
+                        $smalltalk .= ' asDate';
+                    }
+                }
+
+                $smalltalk .= "; ";
             }
+            //Remove the last character using substr
+            $smalltalk = substr($smalltalk, 0, -2);
+            $smalltalk .= '.<br>';
+
+            echo $smalltalk . "<br>";
         }
     }
 
     /**
      * Manually generates collection initialization with records added into itself.
-     *
-     * @param $collection
-     * @param $start
-     * @param $end
-     * @param $variable
+     * @param string $collection <p>
+     * Name for the new collection variable.
+     * </p>
+     * @param string $variable <p>
+     * Name variable being added to the collection.
+     * </p>
+     * @param int $iterations <p>
+     * How many iterations should do loop go trough.
+     * </p>
+     * @param int $start [optional] <p>
+     * Additionally, you can change the starting number of the first entry.
+     * </p>
      * @return string
      */
-    function collectionGenerator($collection, $start, $end, $variable): string
+    function collectionGenerator(string $collection, string $variable, int $iterations, int $start = 1): string
     {
         // Create new collection
         $record = "{$collection} := new Set.<br>{$collection} ";
+
+        // Treat $start as a offset in order to keep the specified number of iterations
+        $end = ($iterations + ($start - 1));
 
         // Add records to collection
         for ($i = $start; $i <= $end; $i++) {
