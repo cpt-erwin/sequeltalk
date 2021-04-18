d1ccfcf76cea22174fd73fd83f7dcc9db6e403ca
Michal Tuček <xtucm003@studenti.czu.cz>
2021-04-18 15:03:30 +0200

    Added possibility to specify custom variable name & edited docs

--- src/Column.php
+++ src/Column.php
@@ -12,15 +12,49 @@ class Column
     /** @var string Smalltalk datatype of column's value */
     public string $dataType;
 
+    /** @var ?string Object's attribute's name. */
+    public ?string $variableName;
+
     /**
      * Column constructor.
-     * @param string $name
-     * @param string $dataType
+     * @param string $name<p>
+     * Column name.
+     * </p>
+     * @param string $dataType <p>
+     * Smalltalk datatype of column's value.<br>
+     * Setting a value for this variable should be done via one of the Sequeltak\SmalltalkDataType constants.
+     * </p>
+     * <p>
+     * For a <b>string value</b> &#9; use <b>SmalltalkDataType::STRING</b>
+     * </p>
+     * <p>
+     * For a <b>numeric value</b> &#9; use <b>SmalltalkDataType::NUMBER</b>
+     * </p>
+     * <p>
+     * For a <b>date value</b> &#9; use <b>SmalltalkDataType::DATE</b>
+     * </p>
+     * <p>
+     * For a <b>collection value</b> &#9; use <b>SmalltalkDataType::SET</b>
+     * </p>
+     * <p>
+     * For a <b>list value</b> &#9; use <b>SmalltalkDataType::LIST</b>
+     * </p>
+     * <p>
+     * For a <b>bag value</b> &#9; use <b>SmalltalkDataType::BAG</b>
+     * </p>
+     * <p>
+     * For a <b>object value</b> &#9; use <b>SmalltalkDataType::OBJECT</b>
+     * </p>
+     * @param ?string $variableName [optional] <p>
+     * Strictly specifies the attribute's name.<br>
+     * If no value is provided it will generate the attribute's name automatically based on column's name.
+     * </p>
      */
-    public function __construct(string $name, string $dataType)
+    public function __construct(string $name, string $dataType, ?string $variableName = null)
     {
         $this->name = $name;
         $this->dataType = $dataType;
+        $this->variableName = $variableName;
     }
 
 
