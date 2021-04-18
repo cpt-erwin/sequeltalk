07028515efb999caa5ac8917ff2b48280f0c33c2
Michal Tuček <xtucm003@studenti.czu.cz>
2021-04-18 12:12:23 +0200

    Implemented .env configuration for PDO DB connection

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
+require './vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']}", $_ENV['DB_USER'], $_ENV['DB_PASS']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     // Handle the PDOException
