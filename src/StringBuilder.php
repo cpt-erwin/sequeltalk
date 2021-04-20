0dae67c4d150e0a75694e377f8c0e4c4c15eb581
Michal Tuček <xtucm003@studenti.czu.cz>
2021-04-20 21:44:02 +0200

    StringBuilder logic changes

--- src/StringBuilder.php
+++ src/StringBuilder.php
@@ -15,30 +15,16 @@ class StringBuilder
     /** @var string Stores content for build. */
     private string $content = '';
 
-    /** @var string Separator in case of joining multiple strings via $this->appendMultiple. */
-    private string $separator;
-
-    /**
-     * StringBuilder constructor.
-     * @param string $separator [optional] <p>
-     * Separator in case of joining multiple strings via $this->appendMultiple.
-     * </p>
-     */
-    public function __construct(string $separator = '')
-    {
-        $this->separator = $separator;
-    }
-
-
     /**
      * Adds provided string into a builder content.
      * @param string $string <p>
      * Partial content.
      * </p>
      */
-    public function append(string $string): void
+    public function append(string $string): StringBuilder
     {
         $this->content .= $string;
+        return $this;
     }
 
     /**
@@ -47,21 +33,10 @@ class StringBuilder
      * Partial content.
      * </p>
      */
-    public function appendLine(string $string): void
+    public function appendLine(string $string): StringBuilder
     {
-        $this->appendMultiple($string, '<br>');
-    }
-
-
-    /**
-     * Adds provided strings into a builder content.
-     * @param string ...$strings <p>
-     * Multiple strings with partial content.
-     * </p>
-     */
-    public function appendMultiple(string ...$strings): void
-    {
-        $this->content .= implode($this->separator, $strings);
+        $this->content .= "$string <br>";
+        return $this;
     }
 
     /**
